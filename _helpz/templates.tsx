A-Archives-and-Forks/mirror-web

commit 05de4c144d40f96d24ec82bfa6b6aae114624479

feat: add copy button to code blocks
Closes: #500
Fixes: #489
Co-authored-by: @qingpeng9802
Signed-off-by: Miao Wang <shankerwangmiao@gmail.com>

_helpz/templates.tsx
@@ -1,11 +1,16 @@
 import { renderToStaticMarkup } from "react-dom/server";
 import React from "react";
 
-export function renderZForm(codeId, inputChoices, inputVars, counterGen) {
+export function renderZForm(
+  codeId: number,
+  inputChoices: string[],
+  inputVars: Record<string, any>,
+  counterGen: () => number,
+) {
   return renderToStaticMarkup(
     <div className="d-flex flex-wrap align-items-center gap-3 mb-3 mt-3 zhelp-form">
       {(() => {
-        const children = [];
+        const children = [] as React.ReactNode[];
         inputChoices.forEach((inputName) => {
           const input = inputVars[inputName];
           const inputId = "zhelp-input-" + counterGen();
@@ -24,30 +29,30 @@ export function renderZForm(codeId, inputChoices, inputVars, counterGen) {
                 defaultValue={(input.option && input.default) || undefined}
               >
                 {input.option &&
-                  Object.entries(input.option).map(
-                    ([optionName, optionValue]) => (
-                      <option
-                        value={optionName}
-                        key={optionName}
-                        {...(() => {
-                          const attrs = {};
-                          optionValue &&
-                            Object.entries(optionValue).forEach(
-                              ([key, value]) => {
-                                if (key !== "default" && key !== "_") {
-                                  attrs[`data-z-set-${key}`] = value;
-                                }
-                              },
-                            );
-                          return attrs;
-                        })()}
-                      >
-                        {optionValue && optionValue["_"]
-                          ? optionValue["_"]
-                          : optionName}
-                      </option>
-                    ),
-                  )}
+                  Object.entries(
+                    input.option as Record<string, Record<string, any>>,
+                  ).map(([optionName, optionValue]) => (
+                    <option
+                      value={optionName}
+                      key={optionName}
+                      {...(() => {
+                        const attrs: Record<string, string> = {};
+                        optionValue &&
+                          Object.entries(optionValue).forEach(
+                            ([key, value]) => {
+                              if (key !== "default" && key !== "_") {
+                                attrs[`data-z-set-${key}`] = value;
+                              }
+                            },
+                          );
+                        return attrs;
+                      })()}
+                    >
+                      {optionValue && optionValue["_"]
+                        ? optionValue["_"]
+                        : optionName}
+                    </option>
+                  ))}
               </select>
             );
           } else if (
@@ -97,5 +102,30 @@ export function renderZForm(codeId, inputChoices, inputVars, counterGen) {
         return children;
       })()}
     </div>,
+  );
+}
+
+export function renderCodeBlock(
+  code: string | null,
+  renderedCode: string,
+  otherAttrs: Record<string, string>,
+) {
+  return renderToStaticMarkup(
+    <pre {...otherAttrs} className="codeblock">
+      <code dangerouslySetInnerHTML={{ __html: renderedCode }}></code>
+      {code != null && <code data-original-code>{code}</code>}
+      <button
+        type="button"
+        className="btn-clipboard"
+        aria-label="Copy to clipboard"
+      >
+        <span>
+          <i>{`{%endraw%}{% fa_svg far.fa-clipboard %}{%raw%}`}</i>
+          <i
+            data-checked
+          >{`{%endraw%}{% fa_svg fas.fa-clipboard-check %}{%raw%}`}</i>
+        </span>
+      </button>
+    </pre>,
   );
 }

_src/entrypoints/help.ts
@@ -104,11 +104,40 @@ const debugRestore = ref(false);
   elem.addEventListener("change", changeHandler);
 });
 
+const attachCopyButton = (preElem: Element, getRawCode: () => string) => {
+  const copyButton = preElem.querySelector("button.btn-clipboard");
+  if (copyButton) {
+    copyButton.addEventListener("click", () => {
+      navigator.clipboard.writeText(getRawCode()).then(
+        () => {
+          copyButton.classList.add("copied");
+          setTimeout(() => {
+            copyButton.classList.remove("copied");
+          }, 2000);
+        },
+        () => {
+          document
+            .getElementById("help-content")
+            .setAttribute("data-cannot-copy", "");
+        },
+      );
+    });
+  }
+};
+
 [...document.querySelectorAll("[data-z-code]")].forEach((elem) => {
   const codeId = parseInt(elem.getAttribute("data-z-code"));
   const lang = elem.getAttribute("data-z-lang");
   const tmpl = new HoganRuntime.Template(ztmpls[codeId]);
-  const originalContent = elem.innerHTML;
+  let codeContainer: Element;
+  let rawCode = "";
+  if (elem.tagName.toLowerCase() === "pre") {
+    codeContainer = elem.querySelector("code");
+    attachCopyButton(elem, () => rawCode);
+  } else {
+    codeContainer = elem;
+  }
+  const originalContent = codeContainer.innerHTML;
   watchEffect(() => {
     const data = { ...flattenData(globalData.value) };
     const endpoint = new URL(data.urlpath as string);
@@ -118,23 +147,39 @@ const debugRestore = ref(false);
       data[k] = v;
     });
     if (debugRestore.value) {
-      elem.innerHTML = originalContent;
+      codeContainer.innerHTML = originalContent;
     } else {
       const renderedConfig = tmpl.render(data);
+      rawCode = renderedConfig;
       if (lang && hljs.getLanguage(lang)) {
-        elem.innerHTML = hljs.highlight(renderedConfig, {
+        codeContainer.innerHTML = hljs.highlight(renderedConfig, {
           language: lang,
         }).value;
       } else if (!lang) {
-        elem.innerHTML = hljs.highlightAuto(renderedConfig).value;
+        codeContainer.innerHTML = hljs.highlightAuto(renderedConfig).value;
       } else {
-        elem.textContent = renderedConfig;
+        codeContainer.textContent = renderedConfig;
       }
     }
   });
 });
 
+[...document.querySelectorAll("pre.codeblock:not([data-z-code])")].forEach(
+  (elem) => {
+    const rawCode = elem.querySelector("code[data-original-code]").textContent;
+    attachCopyButton(elem, () => rawCode);
+  },
+);
+
 document.getElementById("help-content").removeAttribute("data-helpz-not-ready");
+if (
+  typeof navigator !== "undefined" &&
+  navigator.clipboard &&
+  navigator.clipboard.writeText &&
+  typeof navigator.clipboard.writeText === "function"
+) {
+  document.getElementById("help-content").removeAttribute("data-cannot-copy");
+}
 
 document.getElementById("debug-restore").addEventListener("click", () => {
   debugRestore.value = !debugRestore.value;

_src/styles/_bootstrap_vars.scss
@@ -1,3 +1,4 @@
 @import "bootstrap/scss/functions";
 @import "bootstrap/scss/variables";
 @import "bootstrap/scss/variables-dark";
+@import "bootstrap/scss/mixins/border-radius";

_src/styles/help.scss
@@ -1,4 +1,7 @@
 @use "./utils.scss" as utils;
+@use "./bootstrap_vars" as bs;
+@use "./common-defs" as common;
+@use "sass:map";
 
 @include utils.noissue {
   .highlight {
@@ -527,7 +530,8 @@
 }
 
 .form-floating {
-  & > .form-control, & > .form-select {
+  & > .form-control,
+  & > .form-select {
     min-width: 10rem;
   }
   & > .form-switch > input.form-check-input {
@@ -540,4 +544,80 @@
 
 [data-helpz-not-ready] .zhelp-form {
   display: none !important;
+}
+
+$hover-transition-duration: 0.25s;
+$icon-transition-duration: 0.1s;
+
+pre.codeblock {
+  position: relative;
+  & > button.btn-clipboard {
+    [data-cannot-copy] & {
+      display: none;
+    }
+    display: block;
+    line-height: 1;
+    color: var(--bs-body-color);
+    background-color: inherit;
+    border: 0;
+    transition:
+      color $hover-transition-duration,
+      opacity $hover-transition-duration;
+
+    &:hover,
+    &:focus-visible {
+      color: var(--bs-link-hover-color);
+    }
+    margin-top: 0;
+    margin-right: 0;
+    opacity: 0;
+    position: absolute;
+    span {
+      display: inline-block;
+      width: common.$fa_icon_width;
+      height: common.$fa_icon_height;
+      position: relative;
+      i {
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        transition: opacity $icon-transition-duration;
+        /*
+          When the mouse is moved out of the <pre> before
+          the copied class is removed, add the delay to
+          prevent the clipboard icon from flashing back to the
+          original icon during fading out.
+        */
+        transition-delay: $hover-transition-duration;
+      }
+      i[data-checked] {
+        opacity: 0;
+      }
+    }
+    &.copied {
+      i {
+        opacity: 0;
+        transition-delay: 0s;
+      }
+      i[data-checked] {
+        opacity: 1;
+      }
+    }
+    top: 0;
+    right: 0;
+    z-index: 3;
+    padding-top: map.get(bs.$spacers, 2);
+    padding-right: map.get(bs.$spacers, 2);
+    padding-left: 0;
+    padding-bottom: 0;
+  }
+  &:hover > button.btn-clipboard,
+  &:focus-within > button.btn-clipboard,
+  & > button.btn-clipboard.copied {
+    opacity: 1;
+  }
+  code[data-original-code] {
+    display: none;
+  }
 }

_helpz/generate.mjs
@@ -12,7 +12,7 @@ import { VFile } from "vfile";
 import Hogan from "hogan.js";
 import { flattenData } from "../_src/lib/helpz-libs.mjs";
 import hljs from "highlight.js";
-import { renderZForm } from "./templates.jsx";
+import { renderZForm, renderCodeBlock } from "./templates.tsx";
 
 const options = parseArgs({
   options: {
@@ -186,18 +186,23 @@ function getRenderContext(globalVars, zconf, inputVars) {
   return data;
 }
 
+function highlightCode(code, lang) {
+  let highlighted = "";
+  if (lang && hljs.getLanguage(lang)) {
+    highlighted = hljs.highlight(code, { language: lang }).value;
+  } else if (!lang) {
+    highlighted = hljs.highlightAuto(code).value;
+  } else {
+    highlighted = hljs.highlight(code, { language: "plaintext" }).value;
+  }
+  return highlighted;
+}
+
 function renderTemplate(tmpl, globalVars, zconf, inputVars, lang) {
   const renderContext = getRenderContext(globalVars, zconf, inputVars);
   const compiledTemplate = Hogan.compile(tmpl, { asString: true });
   const renderedConfig = Hogan.compile(tmpl).render(renderContext);
-  let highlighted = "";
-  if (lang && hljs.getLanguage(lang)) {
-    highlighted = hljs.highlight(renderedConfig, { language: lang }).value;
-  } else if (!lang) {
-    highlighted = hljs.highlightAuto(renderedConfig).value;
-  } else {
-    highlighted = hljs.escapeHTML(renderedConfig);
-  }
+  const highlighted = highlightCode(renderedConfig, lang);
   return { compiled: compiledTemplate, rendered: highlighted };
 }
 
@@ -378,10 +383,12 @@ enablePages.forEach((page) => {
                   () => inputCounter++,
                 ) + "\n";
             }
-            renderedHTML +=
-              `<pre data-z-code="${templateId}" ${directiveOptions.lang ? `data-z-lang="${directiveOptions.lang}" ` : ""}>\n` +
-              rendered.trim() +
-              "\n</pre>";
+            renderedHTML += renderCodeBlock(null, rendered, {
+              "data-z-code": templateId.toString(),
+              ...(directiveOptions.lang
+                ? { "data-z-lang": directiveOptions.lang }
+                : {}),
+            });
           }
           node.type = "html";
           node.value = renderedHTML;
@@ -391,6 +398,15 @@ enablePages.forEach((page) => {
         }
       },
     );
+    visitor.visit(mdast, "code", (node) => {
+      const highlighted = highlightCode(node.value, node.lang);
+      node.type = "html";
+      node.value = renderCodeBlock(node.value, highlighted, {});
+      delete node.lang;
+      node.children = [];
+      node.position = null;
+      return visitor.SKIP;
+    });
     const modifiedContent = toMarkdown(mdast);
     mdContent.push(modifiedContent);
   });

_layouts/help.html
@@ -41,7 +41,7 @@ jsEntry: help
 					</form>
 				</div>
 				<button id="debug-restore" style="display: none;">debug-restore</button>
-				<div id="help-content" data-helpz-not-ready>
+				<div id="help-content" data-helpz-not-ready data-cannot-copy>
 					{{ content }}
 				</div>
 			</div><!-- help content -->

_src/styles/_common-defs.scss
@@ -1,0 +1,2 @@
+$fa_icon_width: 1em;
+$fa_icon_height: 1.42857143em;

_src/styles/global.scss
@@ -8,6 +8,8 @@
   $link-hover-decoration: none
 );
 
+@use "./common-defs" as common;
+
 $color_thu_purple_dark: #c353d3;
 $footer_color: #384452;
 
@@ -122,8 +124,8 @@ h6,
 
 svg.icon {
   display: inline-block;
-  width: 1em;
-  height: 1.42857143em;
+  width: common.$fa_icon_width;
+  height: common.$fa_icon_height;
   fill: currentColor;
   vertical-align: bottom;
 }

commit 0c49977a01fdb42bde62eac2e3de3b1235f7cff4

copy btn: prevent scrolling with code content
Fixes: 05de4c144d40 ("feat: add copy button to code blocks")
Signed-off-by: Miao Wang <shankerwangmiao@gmail.com>

_helpz/templates.tsx
@@ -111,9 +111,11 @@ export function renderCodeBlock(
   otherAttrs: Record<string, string>,
 ) {
   return renderToStaticMarkup(
-    <pre {...otherAttrs} className="codeblock">
-      <code dangerouslySetInnerHTML={{ __html: renderedCode }}></code>
-      {code != null && <code data-original-code>{code}</code>}
+    <div className="codeblock">
+      <pre {...otherAttrs}>
+        <code dangerouslySetInnerHTML={{ __html: renderedCode }}></code>
+        {code != null && <code data-original-code>{code}</code>}
+      </pre>
       <button
         type="button"
         className="btn-clipboard"
@@ -126,6 +128,6 @@ export function renderCodeBlock(
           >{`{%endraw%}{% fa_svg fas.fa-clipboard-check %}{%raw%}`}</i>
         </span>
       </button>
-    </pre>,
+    </div>,
   );
 }

_src/entrypoints/help.ts
@@ -104,8 +104,8 @@ const debugRestore = ref(false);
   elem.addEventListener("change", changeHandler);
 });
 
-const attachCopyButton = (preElem: Element, getRawCode: () => string) => {
-  const copyButton = preElem.querySelector("button.btn-clipboard");
+const attachCopyButton = (containerElem: Element, getRawCode: () => string) => {
+  const copyButton = containerElem.querySelector("button.btn-clipboard");
   if (copyButton) {
     copyButton.addEventListener("click", () => {
       navigator.clipboard.writeText(getRawCode()).then(
@@ -133,7 +133,7 @@ const attachCopyButton = (preElem: Element, getRawCode: () => string) => {
   let rawCode = "";
   if (elem.tagName.toLowerCase() === "pre") {
     codeContainer = elem.querySelector("code");
-    attachCopyButton(elem, () => rawCode);
+    attachCopyButton(elem.parentElement, () => rawCode);
   } else {
     codeContainer = elem;
   }
@@ -166,10 +166,10 @@ const attachCopyButton = (preElem: Element, getRawCode: () => string) => {
   });
 });
 
-[...document.querySelectorAll("pre.codeblock:not([data-z-code])")].forEach(
+[...document.querySelectorAll(".codeblock > pre:not([data-z-code])")].forEach(
   (elem) => {
     const rawCode = elem.querySelector("code[data-original-code]").textContent;
-    attachCopyButton(elem, () => rawCode);
+    attachCopyButton(elem.parentElement, () => rawCode);
   },
 );
 

_src/styles/_bootstrap_vars.scss
@@ -1,4 +1,5 @@
 @import "bootstrap/scss/functions";
 @import "bootstrap/scss/variables";
+@import "bootstrap/scss/maps";
 @import "bootstrap/scss/variables-dark";
 @import "bootstrap/scss/mixins/border-radius";

_src/styles/help.scss
@@ -552,7 +552,7 @@
 $hover-transition-duration: 0.25s;
 $icon-transition-duration: 0.1s;
 
-pre.codeblock {
+div.codeblock {
   position: relative;
   & > button.btn-clipboard {
     [data-cannot-copy] & {
@@ -561,7 +561,7 @@ pre.codeblock {
     display: block;
     line-height: 1;
     color: var(--bs-body-color);
-    background-color: inherit;
+    background-color: map.get(bs.$utilities-bg-subtle, "secondary-subtle");
     border: 0;
     transition:
       color $hover-transition-duration,
@@ -571,8 +571,12 @@ pre.codeblock {
     &:focus-visible {
       color: var(--bs-link-hover-color);
     }
-    margin-top: 0;
-    margin-right: 0;
+    margin-top: calc(
+      var(--#{bs.$prefix}border-width) + map.get(bs.$spacers, 2)
+    );
+    margin-right: calc(
+      var(--#{bs.$prefix}border-width) + map.get(bs.$spacers, 2)
+    );
     opacity: 0;
     position: absolute;
     span {
@@ -610,10 +614,7 @@ pre.codeblock {
     top: 0;
     right: 0;
     z-index: 3;
-    padding-top: map.get(bs.$spacers, 2);
-    padding-right: map.get(bs.$spacers, 2);
-    padding-left: 0;
-    padding-bottom: 0;
+    padding: 0;
   }
   &:hover > button.btn-clipboard,
   &:focus-within > button.btn-clipboard,